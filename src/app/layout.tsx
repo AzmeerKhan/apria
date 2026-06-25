13ae1fc62cb6dfbffd45fbb9ed4be8b9b2ebd949
Azmeer Khan <azmeerabrar.khan@gmail.com>
2026-06-25 23:48:59 +0500

    minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "APRIA - Professional Accounting & Financial Advisory",
+    default: "Apria Accountants",
     template: "%s | APRIA",
   },
   icons: {
