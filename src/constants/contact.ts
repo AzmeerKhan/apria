b8f78072ebc9cab5b7c554ad40c417cb95961c2e
Azmeer Khan <azmeerabrar.khan@gmail.com>
2026-06-26 21:33:05 +0500

    fix location

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -6,4 +6,4 @@ export const CONTACT_WHATSAPP_HREF = "https://wa.me/447599598568";
 export const CONTACT_LOCATION = "241 Manningham Lane, Bradford, BD8 7ER";
 
 export const CONTACT_MAP_URL =
-  "https://maps.google.com/maps?q=53.805529090563745,-1.7646422584869066&z=15&output=embed";
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2356.0987396351975!2d-1.7643811999999996!3d53.805526699999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487be6ab6232a311%3A0x2448978e9b6c8731!2s241%20Manningham%20Ln%2C%20Manningham%2C%20Bradford%20BD8%207ER%2C%20UK!5e0!3m2!1sen!2s!4v1782491457766!5m2!1sen!2s";
